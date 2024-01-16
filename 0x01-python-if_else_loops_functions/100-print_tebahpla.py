536a6af28fbb06de033b7312d5c7f3cb6e108881
Brian-Wagura <briankiraguri@gmail.com>
2024-01-16 08:56:31 -0800

    Remove at position

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{0}{1}".format(chr(i),chr(i-32)), end="")
+for i in range(25, -1, -1):
+    print("{0}{1}".format(chr(122 - 2*i), chr(89 - 2*i)), end="")
