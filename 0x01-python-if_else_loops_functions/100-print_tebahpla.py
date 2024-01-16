7accf0562605bb5da001485cb532601dcbf73521
Brian-Wagura <briankiraguri@gmail.com>
2024-01-16 09:16:22 -0800

    Correct no 2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{0}{1}".format(chr(122 - 2*i), chr(89 - 2*i)), end="")
+for i in range(ord('z'), ord('A') - 1, -1):
+    print("{0}".format(chr(i), end=""))
