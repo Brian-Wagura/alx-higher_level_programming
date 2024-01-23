c6bbd87c630fd3f3814e04d4c59f3d54e022e8a9
Brian-Wagura <briankiraguri@gmail.com>
2024-01-23 00:44:52 -0800

    Correct functions

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add = (lambda x, y: x + y)
-    sub = (lambda x, y: x - y)
+    add = magic_calculation_102.__dict__['add']
+    sub = magic_calculation_102.__dict__['sub']
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
