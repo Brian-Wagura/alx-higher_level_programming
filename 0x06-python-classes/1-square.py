f2c4b3a809710af3582389ce6d0240ebd56701cd
Brian-Wagura <briankiraguri@gmail.com>
2024-02-06 10:51:31 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square:
+    """a square"""
+    def __init__(self, size):
+        """defines private instance attribute size"""
+        self.__size = size
