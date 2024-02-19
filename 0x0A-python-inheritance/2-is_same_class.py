ab326574d098516b5cfccf3c1f224ddb60ab6ba8
Brian-Wagura <briankiraguri@gmail.com>
2024-02-19 09:57:20 -0800

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Exact same object
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns:True - if is an instance of object
+            False - otherwise
+    """
+    return (type(obj) == a_class)
