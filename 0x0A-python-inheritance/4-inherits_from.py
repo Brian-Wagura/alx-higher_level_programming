f3f1a1a36c327adcffc3f26698a1543cd354c892
Brian-Wagura <briankiraguri@gmail.com>
2024-02-19 10:21:31 -0800

    Remove extra code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     Returns:True - if object is an instance of class inherited
             False - otherwise
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class)
