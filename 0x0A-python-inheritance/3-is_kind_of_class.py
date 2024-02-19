e91a7bed47a38b8bd8ead962c55ba0058e17388a
Brian-Wagura <briankiraguri@gmail.com>
2024-02-19 10:06:03 -0800

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns:True - if the object is an instance of paren/child class
+            False - otherwise
+    """
+    return (isinstance(obj, a_class))
