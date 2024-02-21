d431be8520d145df9d10fd43a3df65e2d5aabb14
Brian-Wagura <briankiraguri@gmail.com>
2024-02-21 07:49:39 -0800

    Corrected check 8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,8 +8,6 @@ def read_file(filename=""):
     """ Reads a text file """
     try:
         with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                print(line, end='')
-
+            print(f.read(), end='')
     except FilenotFoundError:
         pass
