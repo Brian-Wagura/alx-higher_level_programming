7366083042fbbf349366c8ef0bdc2186d84d9ba4
Brian-Wagura <briankiraguri@gmail.com>
2024-02-21 11:51:10 -0800

    Load, add, save correctly

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,14 +6,14 @@ saves to a file
 
 import sys
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__
-    ('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__(
+            '6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
-    except FilenotFoundError:
+    except FileNotFoundError:
         items = []
 
     items.extend(sys.argv[1:])
