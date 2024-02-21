9118c77abd8c1115ac6d10928023025567eb9cc3
Brian-Wagura <briankiraguri@gmail.com>
2024-02-21 12:01:31 -0800

    Square #1 correctly

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,7 +5,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    Square
+    Representation of a Square
     """
     def __init__(self, size):
+        """ Representation of a Square """
+        self.integer_validator("size", size)
+        self._size = size
         super().__init__(size, size)
+
+    def area(self):
+        """ Area of a Square """
+        return self._size ** 2
