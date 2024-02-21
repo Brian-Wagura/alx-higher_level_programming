9a7720037f3316f2b622377cadc45dd6d0c521e0
Brian-Wagura <briankiraguri@gmail.com>
2024-02-21 12:05:34 -0800

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Representation of a Square
+    """
+    def __init__(self, size):
+        """ Representation of a Square """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self._size = size
