6329fe6273402b34ea03b2ea3f884aaff002e418
Brian-Wagura <briankiraguri@gmail.com>
2024-02-21 13:16:10 -0800

    Documentation

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 """
 Reads from stdin line by line and computes metrics.
+
+This script reads input from standard input (stdin) line by line and computes
+metrics such as total file size, count of different HTTP status codes, and total
+number of lines processed.
+
+Metrics computed:
+- Total file size: Sum of file sizes extracted from each line.
+- HTTP status code count: Counts the occurrence of different HTTP status codes.
+- Total line count: Total number of lines processed.
+
+Usage:
+    This script is intended to be run from the command line. It reads input from
+    standard input (stdin) and prints metrics to standard output (stdout).
+
+    Example usage:
+    $ cat access.log | python3 script.py
+
+    The script reads lines from the access.log file (or any other input passed
+    via stdin) and prints computed metrics to stdout.
 """
 import sys
 from collections import defaultdict
@@ -27,8 +46,7 @@ try:
             if line_count % 10 == 0:
                 print(f"Total file size: {total_file_size}")
                 for code in sorted(status_code_count.keys()):
-                    if code in
-                    ['200', '301', '400', '401', '403', '404', '405', '500']:
+                    if code in ['200', '301', '400', '401', '403', '404', '405', '500']:
                         print(f"{code} : {status_code_count[code]}")
                 print()
 except KeyboardInterrupt:
@@ -36,3 +54,4 @@ except KeyboardInterrupt:
     for code in sorted(status_code_count.keys()):
         if code in ['200', '301', '400', '401', '403', '404', '405', '500']:
             print(f"{code} : {status_code_count[code]}")
+
