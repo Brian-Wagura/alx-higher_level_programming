01c914cc6f78215af7cf5574c01eda1ccd8a5bdc
Brian-Wagura <briankiraguri@gmail.com>
2024-02-21 08:58:35 -0800

    Save Object to a file with Python docstrings

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Writes an Object to a tetx file, using JSON representation
+Writes an Object to a text file, using JSON representation
 """
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ Object to a at text file using JSON representation """
     with open(filename, 'w', encoding="utf-8") as f:
         json.dump(my_obj, f)
